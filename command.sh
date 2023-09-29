aab05604d672ae6fae3a953cf8f598f08f4c3bee
dukeent <122994459+dukentt@users.noreply.github.com>
2023-09-30 05:37:03 +0700

    modify command.sh

--- command.sh
+++ command.sh
@@ -1,41 +1,4 @@
-# Create environment:
-	terraform init	
-# Validate code terraform
-	terraform validate
-# Plan create resource
-	terraform plan -out solution.plan
-# Apply plan to create resource
-	terraform apply solution.plan
-# Destroy resource
-    terraform destroy
-
-# Create eks cluster mat nhieu time => 20p de tao resource hoac destroy resource
-
-# Command kubectl
-    # Check version kubectl
-	kubectl version --short
-    # Config kubectl with eks
-    aws eks --region us-east-1 update-kubeconfig --name udacity-prj3-eks
-    # Check config kubectl
-    kubectl config view
-    # Check node
-    kubectl get nodes
-    # Check pod
-    kubectl get pods
-    # Check service => Get external ip to access api - thay the vao fe environment
-    kubectl get svc
-    # Check deployment
-    kubectl get deployment
-    # Check ingress
-    kubectl get ingress
-    # Access to pod terminal
-    kubectl exec -it backend-feed-6c5dbc878-xlpdx -- /bin/bash
-    # Set new image to deployment
-    
-    kubectl set image deployment/deployment_name container_name=image_name
-
-
-    ## Apply env variables and secrets
+## Apply env variables and secrets
 kubectl apply -f aws-secret.yaml
 kubectl apply -f env-secret.yaml
 kubectl apply -f env-configmap.yaml
