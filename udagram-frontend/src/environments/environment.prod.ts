9bde1136f05078fe6cddc7deda13eeec340e61d7
dukeent <122994459+dukentt@users.noreply.github.com>
2023-09-30 02:11:52 +0700

    update to v7

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: true,
   appName: "Udagram",
   apiHost:
-    "http://a1ed1338447ee4bb299b04171d450cb3-1152466383.us-east-1.elb.amazonaws.com:8080/api/v0",
+    "http://a972549310da24f6ca9705e47708e257-1270579201.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
